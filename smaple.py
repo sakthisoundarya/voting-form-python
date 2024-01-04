c5820523f7da0c139b214665923267456878178b
sakthisoundarya <155607336+sakthisoundarya@users.noreply.github.com>
2024-01-04 12:14:10 +0530

    Add files via upload

---
+++ smaple.py
@@ -0,0 +1,22 @@
+# a = int(input("Enter a? "))
+# b = int(input("Enter b? "))
+# c = int(input("Enter c? "))
+# if a>b and a>c:
+#     print("a is largest")
+# elif b>a and b>c:
+#     print("b is largest")
+# else:
+#     print("c is largest")
+#.
+
+# age=int(input("enter a age"))
+# if age >18:
+#     print('you eligble for vote')
+# elif age <=18:
+#     print("you not eligible for vote")
+
+# a=int(input("enter a number"))
+# if a%2==0:
+#     print("this even num")
+# else:
+#     print("it is odd num")
